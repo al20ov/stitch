be9b8b0bb9c4f94bed2a550744498cf9a40ce8cf
al20ov <nicolas.antauf@gmail.com>
2026-02-15 23:06:17 +0100

    add: output/display widget with dimensions and label

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import sys
 
 import gi
 
+from output_widget import OutputWidget
+
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Adw, GObject, Gtk
@@ -30,19 +32,19 @@ class StitchWindow(Adw.ApplicationWindow):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.init_template()
-        self.label = Gtk.Label(label="hello")
-        self.fixed.put(self.label, 0, 0)
+        self.output_widget0 = OutputWidget()
+        self.fixed.put(self.output_widget0, 0, 0)
 
         self.controller: Gtk.GestureDrag = Gtk.GestureDrag()
         self.controller.connect("drag-begin", self.on_begin)
         self.controller.connect("drag-update", self.on_update)
-        self.label.add_controller(self.controller)
+        self.output_widget0.add_controller(self.controller)
 
     def on_begin(self, controller, start_x, start_y):
         self.diff = tuple(
             map(
                 operator.sub,
-                self.fixed.get_child_position(self.label),
+                self.fixed.get_child_position(self.output_widget0),
                 self.get_fixed_pos(),
             )
         )
@@ -50,11 +52,11 @@ class StitchWindow(Adw.ApplicationWindow):
     def on_update(self, controller, offset_x, offset_y):
         move = map(operator.add, self.get_fixed_pos(), self.diff)
 
-        max_width = self.fixed.get_width() - self.label.get_width()
-        max_height = self.fixed.get_height() - self.label.get_height()
+        max_width = self.fixed.get_width() - self.output_widget0.get_width()
+        max_height = self.fixed.get_height() - self.output_widget0.get_height()
         move_x, move_y = map(min, (max_width, max_height), map(max, (0, 0), move))
 
-        self.fixed.move(self.label, move_x, move_y)
+        self.fixed.move(self.output_widget0, move_x, move_y)
 
     def get_fixed_pos(self):
         pointer = self.get_display().get_default_seat().get_pointer()
---
+++ src/output_widget.py
@@ -0,0 +1,18 @@
+import gi
+
+gi.require_version("Gtk", "4.0")
+
+from gi.repository import GObject, Gtk
+
+
+@Gtk.Template(filename="src/output_widget.ui")
+class OutputWidget(Gtk.Box):
+    __gtype_name__ = "output_widget"
+
+    output_name = GObject.Property(type=str, default="Text")
+    width = GObject.Property(type=int, default=100)
+    height = GObject.Property(type=int, default=100)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.init_template()
